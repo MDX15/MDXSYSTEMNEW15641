3fd905a9c9dd60f3c9e1f0c237ec1691846a5272
MDX15 <45093539+MDX15@users.noreply.github.com>
2018-12-01 21:24:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2841,7 +2841,7 @@ message.channel.stopTyping()
      let embed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setColor("#9B59B6")
-  .addField(" ** :gear: Server Support :gear: **" , "  **https://discord.gg/TZ3dcyC**")
+  .addField(" ** :gear: Server Support :gear: **" , "  **https://discord.gg/asJPw5v**")
      
      
   message.channel.sendEmbed(embed);
